bca5db7881883d69d5e47ceeba6fadb92fb19de4
Sameeha Zahid <sam.zahid103@icloud.com>
2024-04-26 18:05:05 +0100

    Creating the HorseRaceGUI class

---
+++ PART2/src/HorseRaceGUI.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.PrintStream;
+import java.util.ArrayList;
+
+import javax.swing.event.*;
+
+public class HorseRaceGUI extends JFrame {
+    
+}
