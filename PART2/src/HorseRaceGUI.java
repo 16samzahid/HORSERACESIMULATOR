8976141954f65b2e1dc1f301a8e5fa7052c01778
Sameeha Zahid <sam.zahid103@icloud.com>
2024-04-26 18:47:51 +0100

    Adding the text area for the race to be printed to the GUI

--- PART2/src/HorseRaceGUI.java
+++ PART2/src/HorseRaceGUI.java
@@ -7,5 +7,22 @@ import java.util.ArrayList;
 import javax.swing.event.*;
 
 public class HorseRaceGUI extends JFrame {
-    
+    JTextArea raceResults;
+    HorseRaceGUI() {
+
+        //new race textarea where the race will be displayed
+        raceResults = new JTextArea();
+        raceResults.setEditable(false);
+        raceResults.setBackground(Color.WHITE);
+        raceResults.setForeground(Color.BLACK);
+        raceResults.setFont(new Font("Monospaced", Font.PLAIN, 20));
+        JScrollPane scroll = new JScrollPane(raceResults);
+        //ensure
+        scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        //scroll.setPreferredSize(new Dimension(800, 200));
+
+        //redirecting the output stream to the textarea
+        PrintStream printStream = new PrintStream(new CustomOutputStream(raceResults));
+        System.setOut(printStream);
+    }
 }
