bd46866846affd10c398ba7f683e9b8e8014f24a
Sameeha Zahid <sam.zahid103@icloud.com>
2024-04-26 13:26:39 +0100

    Initial commit with Horse and Race class unedited

---
+++ PART1/src/Horse.java
@@ -0,0 +1,10 @@
+/**
+ * The Horse class represents the horses in the race
+ * It has a name and symbol to show its status and methods that allow it to do things
+ * @author Sameeha Zahid
+ * @version Version 1 - 26/03/2024
+ */
+public class Horse
+{
+    
+}
